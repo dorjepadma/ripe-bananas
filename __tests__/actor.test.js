62ed3c6008a8d17843d0d997a9da8a3b3c554ab9
dorjepadma <dorjepadma@gmail.com>
2020-03-26 22:44:23 -0700

    create actor working

---
+++ __tests__/actor.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+require('../lib/db/data-helpers');
+
+describe('actor routes', () => {
+  it('creates an actor', () => {
+    
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'Jessie Big Grin',
+        DOB: '12/4/71',
+        POB: 'Chautauqua, Tennessee'
+      },
+      )
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
+          name: 'Jessie Big Grin',
+          DOB: '12/4/71',
+          POB: 'Chautauqua, Tennessee'
+        });
+      });
+  });
+});
