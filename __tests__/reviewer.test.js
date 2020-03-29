26256faa2d455e7bfa08fda5acb4cf7daee8b51e
dorjepadma <dorjepadma@gmail.com>
2020-03-29 16:11:15 -0700

    basic reviewer slice up

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -21,15 +21,34 @@ describe('reviewer routes', () => {
         });
       });
   });
+  it('gets an reviewer by id', async() => {
+    const reviewer = await getReviewer();
 
-  // it('deletes a reviewer by id', async() => {
-  //   const reviewer = await getReviewer();
-    
-  //   return request(app)
-  //     .delete(`/api/v1/reviewer/${reviewer._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual(reviewer);
-     
-  //     });
-  // });
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toContainEqual(reviewer);
+      });
+  });
+
+  it('gets all reviewers', async() => {
+    const reviewer = await getReviewer();
+
+    return request(app)
+      .get('/api/v1/reviewers')
+      .then(res => {
+        expect(res.body).toContainEqual(reviewer);
+      });
+  });
+
+  it('deletes a reviewer by id', async() => {
+    const reviewer = await getReviewer();
+
+    return request(app)
+      .delete(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual(reviewer);
+      });
+  });
 });
+
